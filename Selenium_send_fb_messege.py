8ea8b667eef82dd5d5693c0e3a3bcd66a57a560f
takobell1988 <59435144+takobell1988@users.noreply.github.com>
2020-05-05 17:16:01 +0300

    cache bot text response

--- Selenium_send_fb_messege.py
+++ Selenium_send_fb_messege.py
@@ -9,8 +9,8 @@ def facebook_login():
     ### OPEN TICTUK DEMO URL ###
     Chrome_driver.get('https://www.facebook.com/tictukDemo/')
     ### ENTER FACEBOOK CREDENTIALS ###
-    Chrome_driver.find_element_by_id("email").send_keys('**********@********')
-    Chrome_driver.find_element_by_id("pass").send_keys('*********')
+    Chrome_driver.find_element_by_id("email").send_keys('ofirtako@gmail.com')
+    Chrome_driver.find_element_by_id("pass").send_keys('bananasplit1987')
     Chrome_driver.find_element_by_id("u_0_3").click()
     sleep(20)
     ### CLICK ON SEND MESSEGE ##
@@ -19,5 +19,11 @@ def facebook_login():
     ### SEND HELLO MESSEGE ###
     Chrome_driver.find_element_by_xpath('/html/body/div[1]/div[7]/div[1]/div/div/div[4]/div/div[1]/div/div/div/div/div/div/div/div[2]/div[4]/div/div/div/span/div/div/div[2]/div/div/div/div').send_keys('hello'+Keys.ENTER)
     sleep(10)
+    ### BOT RESPONSE ###
+    response = Chrome_driver.find_element_by_xpath('/html/body/div[1]/div[7]/div[1]/div/div/div[4]/div/div[1]/div/div/div/div/div/div[1]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[3]/div[2]/div/div/div/div/div[1]/div/div[2]/div/div/div').text
+    print(response)
+    sleep(10)
     ### CLOSE CHROME ###
     Chrome_driver.close()
+
+facebook_login()
